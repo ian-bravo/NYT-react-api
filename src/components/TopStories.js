b44e17ca2fee57401fea53d1d2f7419e35083487
ian-bravo <bravo.ian@gmail.com>
2023-12-12 11:10:11 -0800

    add reducer hook

--- src/components/TopStories.js
+++ src/components/TopStories.js
@@ -1,48 +1,117 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer} from "react";
+import topStoriesReducer from './../reducers/top-stories-reducer';
+import { getTopStoriesFailure, getTopStoriesSuccess } from '../actions/index';
+
+const initialState = {
+  isLoaded: false,
+  topStories: [],
+  error: null
+};
 
 function TopStories () {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [topStories, setTopStories] = useState([]);
+  const [state, dispatch] = useReducer(topStoriesReducer, initialState);
 
   useEffect(() => {
     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
       .then(response => {
-          if (!response.ok) {
-            throw new Error(`${response.status}: ${response.statusText}`);
-          } else {
-            return response.json()
-          }
-        })
-      .then((jsonifiedResponse) => {
-          setTopStories(jsonifiedResponse.results)
-          setIsLoaded(true)
-        })
+        if (!response.ok) {
+          throw new Error (`response.status}: ${response.statusText}`);
+        } else {
+          return response.json()
+        }
+      })
+      .then((jsonifiedResonse) => {
+        const action = getTopStoriesSuccess(jsonifiedResponse.results)
+        dispatch(action);
+      })
       .catch((error) => {
-        setError(error.message)
-        setIsLoaded(true)
+        const action = getTopStoriesFailure(error.message)
+        dispatch(action);
       });
-    }, [])
-    
-    if (error) {
-      return <h1>Error: {error}</h1>;
-    } else if (!isLoaded) {
-      return <h1>...Loading...</h1>;
-    } else {
-      return (
-        <React.Fragment>
-          <h1>Top Stories</h1>
-          <ul>
-            {topStories.map((article, index) =>
-              <li key={index}>
-                <h3>{article.title}</h3>
-                <p>{article.abstract}</p>
-              </li>
-            )}
-          </ul>
-        </React.Fragment>
-      );
-    }
-  }
+
+  }, [])
+
+  const { error, isLoaded, topStories } = state;
   
-  export default TopStories;
+  if (error) {
+    return<h1>Error: {error}</h1>;
+  } else if (!isLoaded) {
+    return <h1>...Loading...</h1>
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Top Stories</h1>
+        <ul>
+          {topStories.map((article, index) => 
+            <l1 key={index}>
+              <h3>{article.title}</h3>
+              <p>{article.abstract}</p>
+            </l1>
+          )}
+        </ul>
+      </React.Fragment>
+    );
+  }
+}
+
+export default TopStories;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React, { useState, useEffect } from 'react';
+
+// function TopStories () {
+//   const [error, setError] = useState(null);
+//   const [isLoaded, setIsLoaded] = useState(false);
+//   const [topStories, setTopStories] = useState([]);
+
+//   useEffect(() => {
+//     fetch(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${process.env.REACT_APP_API_KEY}`)
+//       .then(response => {
+//           if (!response.ok) {
+//             throw new Error(`${response.status}: ${response.statusText}`);
+//           } else {
+//             return response.json()
+//           }
+//         })
+//       .then((jsonifiedResponse) => {
+//           setTopStories(jsonifiedResponse.results)
+//           setIsLoaded(true)
+//         })
+//       .catch((error) => {
+//         setError(error.message)
+//         setIsLoaded(true)
+//       });
+//     }, [])
+    
+//     if (error) {
+//       return <h1>Error: {error}</h1>;
+//     } else if (!isLoaded) {
+//       return <h1>...Loading...</h1>;
+//     } else {
+//       return (
+//         <React.Fragment>
+//           <h1>Top Stories</h1>
+//           <ul>
+//             {topStories.map((article, index) =>
+//               <li key={index}>
+//                 <h3>{article.title}</h3>
+//                 <p>{article.abstract}</p>
+//               </li>
+//             )}
+//           </ul>
+//         </React.Fragment>
+//       );
+//     }
+//   }
+  
+//   export default TopStories;
